ee84a7ae2233015a8a5da2c4aa076c234a0ca54e
Dogpa <dogpa0130@gmail.com>
2022-09-05 21:32:24 +0800

    Initial Commit

---
+++ Swift Practice 173 ScrollViewReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 173 ScrollViewReader
+//
+//  Created by Dogpa's MBAir M1 on 2022/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 173 ScrollViewReader/Swift_Practice_173_ScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_173_ScrollViewReaderApp.swift
+//  Swift Practice 173 ScrollViewReader
+//
+//  Created by Dogpa's MBAir M1 on 2022/9/5.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_173_ScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
